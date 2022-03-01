7ba45c7e87050abeefcd5f747247f31cb588cd63
Linus De Meyere <linus@zenjoy.be>
2022-03-02 00:17:20 +0100

    better validation of unique races

--- lib/dovecot/races/race.ex
+++ lib/dovecot/races/race.ex
@@ -20,6 +20,10 @@ defmodule Dovecot.Races.Race do
     |> cast(attrs, [:loft_id, :name, :distance, :release_date, :release_time])
     |> validate_required([:loft_id, :name, :release_date])
     |> validate_number(:distance, greater_than_or_equal_to: 1000, less_than_or_equal_to: 5_000_000)
+    |> unsafe_validate_unique([:loft_id, :name, :release_date], Dovecot.Repo,
+      error_key: :name,
+      message: "er is al een race met deze naam op dezelfde dag"
+    )
     |> unique_constraint([:loft_id, :name, :release_date])
   end
 end
